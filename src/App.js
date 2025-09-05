f51b5708962cdfef530ffb0989d189d3554e5ecc
tugascrown204 <gajdertevet@hotmail.com>
2025-09-05 21:50:41 +0330

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Clean Code Visualizer</h1>
+      <p>Input your code below for analysis.</p>
+      {/* Future code input and analysis components will go here */}
+    </div>
+  );
+}
+
+export default App;

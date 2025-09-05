5296f27ec486d22d2441e8e028d99df168d00963
tugascrown204 <gajdertevet@hotmail.com>
2025-09-05 21:50:44 +0330

    feat: initial commit for src/components/CodeInput.js

---
+++ src/components/CodeInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CodeInput = () => {
+  return (
+    <textarea placeholder="Paste your code here..." rows="10" cols="50"></textarea>
+  );
+};
+
+export default CodeInput;
